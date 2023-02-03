9d4a48d5db9acbaf0f6ccae4603d71bbc7f73e94
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-02-02 21:02:26 -0500

    Rename from client to wallet
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- coinbase/wallet.py
+++ coinbase/wallet.py
@@ -1,5 +1,5 @@
 # teleprint-me/coinbase - A Python Wrapper for Coinbase
-# Copyright (C) 2021 teleprint.me
+# Copyright (C) 2021 Austin Berrio
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
